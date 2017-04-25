fed9854df2c1133fe5a41c99226d3986da23d318
Thierry Michel <thmichel@gmail.com>
2017-04-25 22:23:37 +0200

    :fire: removed unused files/code for new project

--- src/main.js
+++ src/main.js
@@ -1,19 +1,6 @@
 /**
  * Main JS entry file
  */
-import App from 'App'
-import Cube from 'Cube'
-
 global.debug = true
 
 console.info('Ready! 🚀')
-
-/* eslint-disable no-new */
-const app = new App()
-// const cube = new Cube()
-
-// Ajouter cube à notre app
-for (let i = 0; i < 20; i++) {
-  app.add(new Cube());
-}
-
