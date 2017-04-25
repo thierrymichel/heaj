5a29aa9c8e37271399cf3f5d6042c5779729a6e1
Thierry Michel <thmichel@gmail.com>
2017-04-25 23:20:52 +0200

    :sparkles: added class Game with init onDOMContentLoaded

--- src/main.js
+++ src/main.js
@@ -1,6 +1,50 @@
 /**
  * Main JS entry file
  */
-global.debug = true;
 
-console.info('Ready! 🚀');
+/**
+ * The game
+ *
+ * @class Game
+ */
+class Game {
+  /**
+   * Creates an instance of Game.
+   *
+   * @memberOf Game
+   */
+  constructor() {
+    // Quand le DOM est prêt, on initialise le jeu
+    Game.domReady.then(() => {
+      this.init();
+    });
+  }
+
+  /**
+   * Attend que le DOM soit prêt.
+   *
+   * @readonly
+   * @static
+   * @returns {Promise} statut du chargement
+   * @memberOf Game
+   */
+  static get domReady() {
+    return new Promise((resolve) => {
+      document.addEventListener('DOMContentLoaded', resolve);
+    });
+  }
+
+  /**
+   * Initialise le jeu.
+   *
+   * @returns {undefined}
+   * @memberOf Game
+   */
+  init() {
+    console.info('Game:init! 🚀', this);
+    global.debug = true;
+    document.documentElement.classList.add('ready');
+  }
+}
+
+new Game(); // eslint-disable-line no-new
